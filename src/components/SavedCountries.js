26f60f9a5bda13bc30566a85d2e4d35d770a8312
Cody Fackrell <codyfackrell56@gmail.com>
2023-09-09 22:16:04 -0600

    file cleanup

--- src/components/SavedCountries.js
+++ src/components/SavedCountries.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import DisplayedFlashcard from "./flashcards/DisplayedFlashcard";
 import CountryContext from "../store/countryContext";
 
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import CountrySelector from "../../store/CountrySelector";
+import CountrySelector from "../../assets/CountrySelector";
 import AuthContext from "../../store/authContext";
 
 
